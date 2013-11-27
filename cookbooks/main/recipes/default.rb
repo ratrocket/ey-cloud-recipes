a803537b84edf0f26c5d52d0fea3a049bf3b29aa
Stuart Fletcher <stuart.fletcher@gmail.com>
2013-11-27 12:38:09 -0800

    Readying redis.
    Per instructions at
    https://github.com/ratrocket/ey-cloud-recipes/tree/master/cookbooks/redis
    and somewhat
    https://support.cloud.engineyard.com/entries/21388468-Using-Redis
    and
    https://support.cloud.engineyard.com/entries/20996576-Add-Redis-to-Your-Application

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -63,7 +63,7 @@ include_recipe "sidekiq"
 # include_recipe "resque-scheduler"
 
 #uncomment to run the redis recipe
-#include_recipe "redis"
+include_recipe "redis"
 
 #uncomment to run the api-keys-yml recipe
 # include_recipe "api-keys-yml"
