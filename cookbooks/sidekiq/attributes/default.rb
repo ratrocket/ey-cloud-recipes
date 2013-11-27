80bdf752c1bb2ba42996712464432102af0642fb
Stuart Fletcher <stuart.fletcher@gmail.com>
2013-11-26 23:41:55 -0800

    Setting up sidekiq.

--- cookbooks/sidekiq/attributes/default.rb
+++ cookbooks/sidekiq/attributes/default.rb
@@ -8,20 +8,28 @@ default[:sidekiq] = {
   # unless a utility name is set, in which case, Sidekiq will
   # only be installed on to a utility instance that matches
   # the name
-  :utility_name => 'sidekiq',
-  
+  #:utility_name => 'sidekiq',
+  :utility_name => nil,
+
   # Number of workers (not threads)
   :workers => 1,
   
   # Concurrency
+  # NOTE In development we have the postgres pool set at 25 and setting
+  # the sidekiq concurrency to 25 causes problems.  To fix that I set
+  # the sidekiq concurrency to 15 & that seemed to run smooth.  But it
+  # looks like in production, the EY-written database.yml doesn't even
+  # have a "pool" setting, so I'm not sure if we should lower this or
+  # what?  I guess we'll try 25 and see what happens.
   :concurrency => 25,
-  
+
   # Queues
   :queues => {
     # :queue_name => priority
-    :default => 1
+    :default => 1,
+    :paperclip => 5,
   },
-  
+
   # Verbose
   :verbose => false
 }
